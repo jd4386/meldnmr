2206650f5ceb93e1ab4d241c54f0e4db1883d54f
jd4386 <45505677+jd4386@users.noreply.github.com>
2023-06-23 08:53:33 -0400

    django base app with skeleton html and table model

---
+++ events/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class RequestModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
